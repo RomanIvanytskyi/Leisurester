c1dd9bbf7871496bb6e7d7c6012b91c1b873c384
RomanIvanytskyi <ivanitckij999@gmail.com>
2021-08-08 02:02:01 +0300

    Random generation

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const app = express();
 const bodyParser = require("body-parser");
 const User = require("./Users");
 const Proposition = require("./proposition");
+const leisurense = require("./leisurense");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
@@ -34,7 +35,7 @@ app.post("/postData", async (req, res) => {
 app.post("/deleteData", async (req, res) => {
   console.log(req.body.id);
   await Post.deleteOne({ _id: req.body.id });
-  return res.send("deleted " + req.body.id); 
+  return res.send("deleted " + req.body.id);
 });
 
 app.post("/register", async (req, res) => {
@@ -54,10 +55,6 @@ app.get("/getUsers", (req, res) => {
   User.find({}, (err, found) => {
     res.json(found);
   });
-});
-
-app.listen(4000, () => {
-  console.log("Server has been started");
 });
 
 app.post("/login", (req, res) => {
@@ -91,4 +88,77 @@ app.get("/getProposition", (req, res) => {
   Proposition.find({}, (err, found) => {
     res.json(found);
   });
+});
+
+app.get("/getRandom", async (req, res) => {
+  let idArray = [];
+  let postArray = [];
+
+  await Post.find({}, (err, found) => {
+    if (!found) {
+      res.status(404).json({ err });
+      return;
+    } else {
+      idArray = found.map((item) => {
+        return item._id;
+      });
+      console.log(idArray);
+      return;
+    }
+  });
+
+  Promise.all(
+    getArray(idArray.length - 1).map(async (index) => {
+      let data = await getById(idArray[index]);
+      console.log(data.name);
+      postArray.push({
+        name: data.name,
+        category: data.category,
+        type: data.type,
+        description: data.description,
+        id: data._id,
+        file: data.file,
+      });
+    })
+  ).then(() => {
+    res.send([...postArray]);
+    return;
+  });
+});
+
+const getById = async (id) => {
+  let data;
+  await Post.find({ _id: id }, (err, found) => {
+    if (!found) {
+      data = null;
+    } else {
+      data = found[0];
+    }
+    return;
+  });
+  if (!data) {
+    data = await getById(id);
+  }
+  return data;
+};
+
+const getArray = (max) => {
+  min = 0;
+  let arr = [];
+
+  for (let i = 0; i < 3; i++) {
+    const random = Math.floor(Math.random() * max);
+    if (arr.includes(random, [], "")) {
+      i--;
+      continue;
+    }
+    arr[i] = random;
+  }
+  console.log();
+  console.log(arr);
+  return arr;
+};
+
+app.listen(4000, () => {
+  console.log("Server has been started");
 });
